fe8fc78abc4931212e6e982fdf787a9305e1d8b8
Balder <balderi@tsponline.dk>
2020-04-25 06:48:22 +0200

    Config

--- Thumbnailer/Config.cs
+++ Thumbnailer/Config.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
 using System.Xml;
 using System.Xml.Serialization;
 
@@ -10,11 +6,11 @@ namespace Thumbnailer
 {
     class Config
     {
-        [XmlElement(ElementName ="Columns")]
-        public int Columns { get; set; }
-
         [XmlElement(ElementName = "Rows")]
         public int Rows { get; set; }
+
+        [XmlElement(ElementName ="Columns")]
+        public int Columns { get; set; }
 
         [XmlElement(ElementName = "Width")]
         public int Width { get; set; }
@@ -22,19 +18,59 @@ namespace Thumbnailer
         [XmlElement(ElementName = "Gap")]
         public int Gap { get; set; }
 
-        public void Save()
-        {
+        [XmlElement(ElementName = "BackgroundColor")]
+        public int BackgroundColor { get; set; }
 
+        [XmlElement(ElementName = "InfoColor")]
+        public int InfoColor { get; set; }
+
+        [XmlElement(ElementName = "TimeColor")]
+        public int TimeColor { get; set; }
+
+        [XmlElement(ElementName = "ShadowColor")]
+        public int ShadowColor { get; set; }
+
+        [XmlElement(ElementName = "InfoFont")]
+        public string InfoFont { get; set; }
+
+        [XmlElement(ElementName = "TimeFont")]
+        public string TimeFont { get; set; }
+
+        [XmlElement(ElementName = "InfoFontSize")]
+        public int InfoFontSize { get; set; }
+
+        [XmlElement(ElementName = "TimeFontSize")]
+        public int TimeFontSize { get; set; }
+
+        [XmlElement(ElementName = "InfoChecked")]
+        public bool InfoChecked { get; set; }
+
+        [XmlElement(ElementName = "TimeChecked")]
+        public bool TimeChecked { get; set; }
+
+        [XmlElement(ElementName = "ShadowChecked")]
+        public bool ShadowChecked { get; set; }
+
+        readonly string _defaultPath = "config.xml";
+
+        public void Save(Config config)
+        {
+            SaveAs(config, _defaultPath);
         }
 
-        public void SaveAs()
+        public void SaveAs(Config config, string path)
         {
-
+            var xmls = new XmlSerializer(config.GetType());
+            var writer = new StreamWriter(path);
+            xmls.Serialize(writer, config);
+            writer.Close();
         }
 
-        public void Load()
+        public static Config Load(string path)
         {
-
+            var fs = new FileStream(path, FileMode.Open);
+            var xmls = new XmlSerializer(typeof(Config));
+            return (Config)xmls.Deserialize(fs);
         }
     }
 }
